f5db38a746e3e090e9397cedb78fc55cfd4ea042
gustavo <95720704+profeGusty@users.noreply.github.com>
2025-04-25 09:17:39 -0300

    primer subida de psgian

---
+++ app.js
@@ -0,0 +1,28 @@
+const app = document.getElementById("app");
+let templateHtml = "";
+let listaFrutas = ['🍓','🍌','🍎','🍇','🍎','🍍','🍓','🥝','🥝','🍇','🍌','🍍'];
+let arrayCard = [];
+let arrayFrutas = [];
+
+
+listaFrutas.forEach(fruta =>(
+    templateHtml += `
+    <div class="card">
+        <div class="lado frente"></div>
+        <div class="lado atras">${fruta}</div>
+    </div>
+    `
+));
+
+app.innerHTML = templateHtml;
+
+app.addEventListener('click',(e)=>{
+    let value = e.target.classList.contains('frente')
+    console.log(e.target)
+
+    if(value){
+        let elementCard = e.target.parentElement
+        console.log(elementCard)
+    }
+
+})
